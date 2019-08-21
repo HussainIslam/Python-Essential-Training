d742a91f9786d7cc673d6442578d83491a2afe08
hussain <m.hussainul.islam@gmail.com>
2019-08-21 00:56:47 -0400

    CH13 done

---
+++ CH13_Built-in_Functions/types.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+x = '47'
+y = int(x)
+z = float(x)
+a = abs(y)
+dm = divmod(y, 3)
+im = y + 73j
+im2 = complex(y, 73)
+
+print(f'x is {type(x)}')
+print(f'x is {x}')
+print(f'y is {type(y)}')
+print(f'y is {y}')
+print(f'z is {type(z)}')
+print(f'z is {z}')
+print(f'a is {type(a)}')
+print(f'a is {a}')
+print(f'dm is {type(dm)}')
+print(f'dm is {dm}')
+print(f'im is {type(im)}')
+print(f'im is {im}')
+print(f'im2 is {type(im2)}')
+print(f'im2 is {im2}')
